ecf18c3bc039e971f1336ef5917814adbaa6e0a5
PC <louna.berti@ynov.com>
2025-01-13 14:48:59 +0100

    Ajout du .gitignore et suppression des fichiers non nécessaires

---
+++ Tetris_game/GameGrid .cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris_game
+{
+    public class GameGrid
+    {
+        private readonly int[,] grid;
+        public int Rows {  get; }
+        public int Columns { get; }
+
+        public int this[int r, int c]
+        {
+            get => grid[r, c];
+            set => grid[r, c] = value;
+        }
+
+        public GameGrid (int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+            grid = new int[rows, columns];
+        }
+        public bool IsInside(int r, int c)
+        {
+            return r >= 0 && r < Rows && c >= 0 && c < Columns;
+        }
+    }
+}
